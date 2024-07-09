a3a3b1c9ebe51f5b55c915f7b235ea6b6622492d
Marck Bashaw <marckbashaw@gmail.com>
2024-07-09 14:37:19 -0400

    Skills page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import DaisyUiNavBar from "./DaisyUiNavBar";
 import Footer from './Footer';
+import MERN from '../assets/MERN.jpg'
+import MongoDBpng from '../assets/MongoDBpng.png'
+import Express from '../assets/Express.png'
 
 export default function Skills() {
   return (
@@ -10,7 +13,31 @@ export default function Skills() {
         <nav>
           <DaisyUiNavBar />
         </nav>
-        <div className='flex-1 bg-base-300'>Skills Page</div>
+        <div className='flex-1 bg-base-300 place-self-center lg:w-10/12 mt-6'>
+          <div className="bg-base-300 text-4xl border-b-2 border-secondary lg:mb-10">Skills</div>
+          <div className="flex justify-center my-4">
+            <img
+              src={MERN} alt="MERN" className="lg:w-[1000px] lg:h-[250px] rounded-lg shadow-2xl"
+            />
+          </div>
+          <div className="text-2xl flex mb-10 bg-base-300">
+          I currently specialize in using the MERN tech stack to produce my work.  This includes 
+          MongoDB for a database, utilizing Mongoose as an ODM, Express for back end development, 
+          React for front end development, and Node.js for my JavaScript run-time environment.
+          </div>
+          <div className="flex flex-col">
+            <div className="flex">
+              <img
+                src={MongoDBpng} alt="mongoDB" className="h-[100px] w-[100px]"
+              />
+            </div>
+            <div className="flex">
+              <img
+                src={Express} alt="express.js" className="h-[100px] w-[100px]"
+              />
+            </div>
+          </div>
+        </div>
         <Footer />
       </div>
     </div>
