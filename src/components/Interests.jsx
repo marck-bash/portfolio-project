e1adc5e0cae4e7a7cef7b8f681e97995417d7eaa
Marck Bashaw <marckbashaw@gmail.com>
2024-07-25 23:03:47 -0400

    Add: Added site construction message to the Interests and Projects pages to let new site visitors know that this content is coming soon.

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import DaisyUiNavBar from "./DaisyUiNavBar";
 import HeroDaisyUi from './HomePage';
 import Footer from './Footer';
+import progressBar from '../assets/progressBar.png'
 
 export default function Interests() {
   return (
@@ -11,8 +12,14 @@ export default function Interests() {
         <nav>
           <DaisyUiNavBar />
         </nav>
-        <div className="flex-1 place-self-center lg:mt-20 bg-base-300 lg:px-24 px-2">
+        <div className="flex-1 lg:mt-20 bg-base-300 lg:px-24 px-2">
           <div className="bg-base-300 text-4xl border-b-2 border-secondary lg:mb-10">Interests</div>
+          <div className="text-6xl flex place-content-center">UNDER</div>
+          <div className="text-6xl flex place-content-center mb-8 ">RENOVATION</div>
+          <div className="text-xl flex place-content-center mb-2">PAGE NEARLY READY</div>
+          <div className="flex place-content-center mb-10">
+            <img src={progressBar} alt="In progress" className="h-[200px] w-[260px]"/>
+          </div>
         </div>
         <Footer />
       </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import DaisyUiNavBar from "./DaisyUiNavBar";
 import Footer from './Footer';
+import progressBar from '../assets/progressBar.png'
 
 export default function Projects() {
   return (
@@ -10,8 +11,14 @@ export default function Projects() {
         <nav>
           <DaisyUiNavBar />
         </nav>
-        <div className='flex-1 bg-base-300 place-self-center lg:mt-20 lg:px-24 px-2'>
+        <div className='flex-1 bg-base-300 lg:mt-20 lg:px-24 px-2'>
           <div className="bg-base-300 text-4xl border-b-2 border-secondary lg:mb-10">Projects Page</div>
+          <div className="text-6xl flex place-content-center">UNDER</div>
+          <div className="text-6xl flex place-content-center mb-8 ">RENOVATION</div>
+          <div className="text-xl flex place-content-center mb-2">PAGE NEARLY READY</div>
+          <div className="flex place-content-center mb-10">
+            <img src={progressBar} alt="In progress" className="h-[200px] w-[260px]"/>
+          </div>
         </div>
         <Footer />
       </div>
